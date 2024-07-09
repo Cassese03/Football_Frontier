48f46c3b7492aaf848d55956d89bab0048cd90c3
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-07-09 16:34:44 +0200

    ciao

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:football_app/constants.dart';
+import 'package:football_app/screens/login/login_bloc.dart';
+import 'package:football_app/screens/login/login_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/players_provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:meta_seo/meta_seo.dart';
+import 'package:flutter/foundation.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(MyApp());
+  initializeDateFormatting('it', null);
+  //WidgetsFlutterBinding.ensureInitialized();
+  //Firebase.initializeApp();
+  if (kIsWeb) {
+    MetaSEO().config();
+  }
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider.value(value: PlayersProvider())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: WinnerAnimation(),
+    return BlocProvider(
+      create: (context) => LoginBloc(),
+      child: MaterialApp(
+        title: 'Football Frontier',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: "PlusJakartaSans",
+          scaffoldBackgroundColor: kbackgroundColor,
+          appBarTheme: const AppBarTheme(color: kbackgroundColor),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: kprimaryColor,
+            background: kbackgroundColor,
+          ),
         ),
+        home: const LoginScreen(),
       ),
     );
   }
 }
+// import 'package:flutter/material.dart';
 
-class WinnerAnimation extends StatefulWidget {
-  @override
-  _WinnerAnimationState createState() => _WinnerAnimationState();
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class _WinnerAnimationState extends State<WinnerAnimation> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: WinnerAnimation(),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  void initState() {
-    super.initState();
+// // class WinnerAnimation extends StatefulWidget {
+//   @override
+//   _WinnerAnimationState createState() => _WinnerAnimationState();
+// }
 
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    )..repeat(reverse: true);
+// class _WinnerAnimationState extends State<WinnerAnimation> with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<double> _animation;
 
-    _animation = Tween<double>(begin: 0, end: 24).animate(_controller)
-      ..addListener(() {
-        setState(() {});
-      });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+//     _controller = AnimationController(
+//       duration: const Duration(seconds: 2),
+//       vsync: this,
+//     )..repeat(reverse: true);
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        // Background
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/gif/fuochi.gif'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
+//     _animation = Tween<double>(begin: 0, end: 24).animate(_controller)
+//       ..addListener(() {
+//         setState(() {});
+//       });
+//   }
 
-        // Winner text
-        Center(
-          child: Text(
-            'Winner',
-            style: TextStyle(
-              fontSize: 48 + _animation.value,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-        // Image below the text
-        Positioned(
-          bottom: 0,
-          child: Image.network('https://ws.footballfrontier.it/img/football_profilo_773787716.jpg'),
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: <Widget>[
+//         // Background
+//         Container(
+//           decoration: BoxDecoration(
+//             image: DecorationImage(
+//               image: AssetImage('assets/gif/fuochi.gif'),
+//               fit: BoxFit.cover,
+//             ),
+//           ),
+//         ),
+
+//         // Winner text
+//         Center(
+//           child: Text(
+//             'Winner',
+//             style: TextStyle(
+//               fontSize: 48 + _animation.value,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//         ),
+
+//         // Image below the text
+//         Positioned(
+//           bottom: 0,
+//           child: Image.network('https://ws.footballfrontier.it/img/football_profilo_773787716.jpg'),
+//         ),
+//       ],
+//     );
+//   }
+// }
